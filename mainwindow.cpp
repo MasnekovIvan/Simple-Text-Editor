75ae1c5fa8e28c00b8ff8c252516f37b98d73105
Masnekov Ivan <masnekovivan@gmail.com>
2018-04-17 16:42:47 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,90 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_triggered() // Работа с новым файлом.
+{
+    mFilename = "";
+    ui->textEdit->setPlainText("");
+}
+
+void MainWindow::on_actionOpen_triggered() // Открытие файла в новом окне.
+{
+    QString file = QFileDialog::getOpenFileName(this, "Open a file");
+
+    if(!file.isEmpty())
+    {
+        QFile sFile(file);
+        if(sFile.open(QFile::ReadOnly | QFile::Text))
+        {
+            mFilename = file;
+            QTextStream in(&sFile);
+            QString text = in.readAll();
+            sFile.close();
+
+            ui->textEdit->setPlainText(text);
+        }
+    }
+}
+
+void MainWindow::on_actionSave_triggered() // Сохранение файла.
+{
+    QFile sFile(mFilename); // Проверка на пустой filename.
+    if((mFilename != "") && (sFile.open(QFile::WriteOnly | QFile::Text)))
+    {
+        QTextStream out(&sFile);
+
+        out << ui->textEdit->toPlainText();
+
+        sFile.flush();
+        sFile.close();
+    }
+}
+
+void MainWindow::on_actionSave_as_triggered() // Кнопка сохранения файла с другим именем, расположеним.
+{
+    QString file = QFileDialog::getSaveFileName(this, "Open a file");
+
+    if(!file.isEmpty())
+    {
+        mFilename = file;
+        on_actionSave_triggered();
+    }
+}
+// Исользуются стандартные функции для копирования, вырезания и т.д.
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
